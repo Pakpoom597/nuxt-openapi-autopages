1e1f12b86e0d899da4f44da01720daa7240e20a4
Pakpoom597 <poom597@gmail.com>
2025-06-21 08:38:49 +0700

    chore(style): add nuxtui with tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
+  css: ["~/assets/css/main.css"],
+  ui: {
+    colorMode: false,
+  },
 });
